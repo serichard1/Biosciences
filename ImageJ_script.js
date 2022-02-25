b81569345be5ce924cb98048e8966108e4d59a09
SEBpyGeneco <93990814+SEBpyGeneco@users.noreply.github.com>
2022-02-25 17:26:47 +0100

    Update ImageJ_script.js

--- ImageJ_script.js
+++ ImageJ_script.js
@@ -5,3 +5,22 @@ Script ImageJ _ mitochondria segmentation
 
 
 'use strict';
+
+
+/////// MACRO VOLÉE sur un forum
+
+// imagej-macro "mitochondria" (Herbie G., 03. Nov. 2018)
+// this macro works with plain ImageJ and BioFormats-plugin installed
+requires( "1.52h" );
+filePath = File.openDialog("");
+setOption("BlackBackground", true);
+run("Bio-Formats Importer", "open=["+filePath+"] color_mode=Default view=Hyperstack stack_order=XYCZT");
+run("Z Project...", "projection=[Sum Slices]");
+run("Bandpass Filter...", "filter_large=10 filter_small=1 suppress=None tolerance=5");
+setAutoThreshold("Otsu dark no-reset");
+run("Convert to Mask");
+run("Watershed"); // this operation is optional
+exit();
+// imagej-macro "maximum" (Herbie G., 03. Nov. 2018)
+
+//////////////////////////////////////
