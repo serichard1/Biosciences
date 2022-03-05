91af633759cc4d941eb7aa27115d286f579ea183
Bordeterre <nikonekoelgato@gmail.com>
2022-03-06 00:14:52 +0100

    Add files via upload
    Automatisation (avec quelques améliorations) du protocole de segmentation semi-automatique
    On finit avec un stack de 2 images : l'image d'origine et l'image d'origine+des masques blancs. On peut corriger ces masques en dessinant par dessus.

---
+++ Pre_segmentation.js
@@ -0,0 +1,42 @@
+//////////////////////////////////////// PARAMETERS ////////////////////////////////////////
+const radius = 15; // Min filter
+const sigma = 10; // Gaussian blurr filter
+
+const min_size = 3000;
+//////////////////////////////////////// IMPORTS ////////////////////////////////////////
+const IJ_PLUGINS = IJ.getDir('plugins');
+load(`${IJ_PLUGINS}/crazybio/nashorn_polyfill.js`);
+load(`${IJ_PLUGINS}/crazybio/utils-ij.js`);
+//////////////////////////////////////// FUNCTIONS ////////////////////////////////////////
+
+//////////////////////////////////////// MAIN ////////////////////////////////////////
+// Duplicate
+let imp = IJ.getImage();
+let mask = imp.duplicate();
+
+IJ.run(mask, "Invert", "");
+
+// Blur
+IJ.run(mask, "Minimum...", "radius="+radius.toString());
+IJ.run(mask, "Gaussian Blur...", "sigma="+ sigma.toString());
+
+// Threshold
+IJ.setAutoThreshold(mask, "Yen");
+IJ.run(mask, "Convert to Mask", "");
+// Fill holes
+IJ.run(mask, "Fill Holes", "");
+// Delete Smaller particles
+IJ.run(mask, "Analyze Particles...", "size=" + min_size.toString()+ "-Infinity show=Masks display clear");
+mask = IJ.getImage();
+
+//Display
+IJ.run(mask, "16-bit", "");
+IJ.run(mask, "Multiply...", "value=257");
+mask.hide();
+mask = ImageCalculator.run(imp, mask, "OR create");
+
+const out = ImagesToStack.run([imp,mask]);
+out.show();
+mask.close();
+
+
